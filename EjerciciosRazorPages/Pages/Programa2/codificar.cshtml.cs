af3e02a9418251ff5900cb1eec26f0b9b5d38224
IDGS-802-23002072 <omarcorderop25@gmail.com>
2026-05-20 22:46:43 -0600

    Logica - Proyecto 2

--- EjerciciosRazorPages/Pages/Programa2/codificar.cshtml.cs
+++ EjerciciosRazorPages/Pages/Programa2/codificar.cshtml.cs
@@ -5,8 +5,72 @@ namespace EjerciciosRazorPages.Pages.Programa2
 {
     public class codificarModel : PageModel
     {
+        [BindProperty]
+        public string mensaje { get; set; } = "";
+
+        [BindProperty]
+        public string factorN { get; set; } = "";
+
+        public string msjCodificado { get; set; } = "";
+        public string msjDecodificado { get; set; } = "";
+
+        private string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            int n = Convert.ToInt32(factorN);
+
+            for (int i = 0; i < mensaje.Length; i++)
+            {
+                if (mensaje[i] != ' ')
+                {
+                    int posicion = alfabeto.IndexOf(char.ToUpper(mensaje[i]));
+
+                    if (posicion != -1)
+                    {
+                        int nuevaPosicion = (posicion + n) % alfabeto.Length;
+
+                        msjCodificado += alfabeto[nuevaPosicion];
+                    }
+                    else {
+                        msjCodificado += mensaje[i];                    
+                    }
+                }
+                else
+                {
+                    msjCodificado += " ";
+                }
+            }
+        }
+
+        public void decodificar() {
+            int n = Convert.ToInt32(factorN);
+
+            for (int i = 0; i < msjCodificado.Length; i++)
+            {
+                if (msjCodificado[i] != ' ')
+                {
+                    int posicion = alfabeto.IndexOf(char.ToUpper(msjCodificado[i]));
+
+                    if (posicion != -1)
+                    {
+                        int nuevaPosicion = (posicion - n) % alfabeto.Length;
+
+                        msjDecodificado += alfabeto[nuevaPosicion];
+                    }
+                    else
+                    {
+                        msjDecodificado += msjCodificado[i];
+                    }
+                }
+                else
+                {
+                    msjDecodificado += " ";
+                }
+            }
         }
     }
 }
