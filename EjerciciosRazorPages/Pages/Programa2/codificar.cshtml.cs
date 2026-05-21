6dc923538e71ca71a38658b4cd02f73be55b5fee
IDGS-802-23002072 <omarcorderop25@gmail.com>
2026-05-20 21:54:00 -0600

    Inicio Programa 2 - Codificacion

---
+++ EjerciciosRazorPages/Pages/Programa2/codificar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EjerciciosRazorPages.Pages.Programa2
+{
+    public class codificarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
