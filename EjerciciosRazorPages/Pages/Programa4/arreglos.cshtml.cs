7e40ba744f204dbd6044edfc7022f960ee6d1146
IDGS-802-23002072 <omarcorderop25@gmail.com>
2026-05-21 10:33:36 -0600

    Inicio Programa 4 - Arreglos

---
+++ EjerciciosRazorPages/Pages/Programa4/arreglos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EjerciciosRazorPages.Pages.Programa4
+{
+    public class arreglosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
