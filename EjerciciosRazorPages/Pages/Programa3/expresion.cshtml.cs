b4f75823e13354c1f765da9a52c186668dc13a37
IDGS-802-23002072 <omarcorderop25@gmail.com>
2026-05-21 00:52:04 -0600

    Inicio - Programa 3 Evaluar expresion

---
+++ EjerciciosRazorPages/Pages/Programa3/expresion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EjerciciosRazorPages.Pages.Programa3
+{
+    public class expresionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
